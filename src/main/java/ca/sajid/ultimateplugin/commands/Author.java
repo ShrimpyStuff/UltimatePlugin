43c6be3fd1e92228e7e1500ff8ea6cb6d8cd0505
ShrimpyStuff <sajidmonowar11@gmail.com>
2020-11-22 17:39:09 -0500

    Working update to the Author Command

--- src/main/java/ca/sajid/ultimateplugin/commands/Author.java
+++ src/main/java/ca/sajid/ultimateplugin/commands/Author.java
@@ -12,10 +12,10 @@ public class Author implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender instanceof ConsoleCommandSender) {
-            sender.sendMessage("Authors are " + ChatColor.DARK_AQUA + "ElCholoGamer and ServantChild");
+            sender.sendMessage("Authors are ElCholoGamer and ServantChild");
         } else {
             Player player = (Player) sender;
-            player.sendTitle("Authors are " + ChatColor.DARK_AQUA + "ElCholoGamer and ServantChild", null, 1, 5, 1);
+            player.sendTitle("Authors are", ChatColor.DARK_AQUA + "ElCholoGamer and ServantChild", 10, 50, 10);
         }
 
         return true;
