d09064d040b7f0cecb399d89e8f95b7ab6980e2c
ShrimpyStuff <sajidmonowar11@gmail.com>
2020-11-22 15:31:14 -0500

    Update to the Author Command

--- src/main/java/ca/sajid/ultimateplugin/commands/Author.java
+++ src/main/java/ca/sajid/ultimateplugin/commands/Author.java
@@ -4,12 +4,15 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Author implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        sender.sendMessage("Authors are " + ChatColor.DARK_AQUA + "ElCholoGamer and ServantChild");
+        //sender.sendMessage("Authors are " + ChatColor.DARK_AQUA + "ElCholoGamer and ServantChild");
+        Player player = (Player) sender;
+        player.sendTitle("Authors are " + ChatColor.DARK_AQUA + "ElCholoGamer and ServantChild", null, 1, 5, 1);
 
         return true;
     }
