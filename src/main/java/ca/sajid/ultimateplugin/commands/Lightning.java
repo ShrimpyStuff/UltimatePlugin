a64105d5d7a3ef8bbfacb9f1e19f20ed338d03da
ShrimpyStuff <sajidmonowar11@gmail.com>
2020-11-21 21:34:58 -0500

    Update

--- src/main/java/ca/sajid/ultimateplugin/commands/Lightning.java
+++ src/main/java/ca/sajid/ultimateplugin/commands/Lightning.java
@@ -1,5 +1,6 @@
 package ca.sajid.ultimateplugin.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,6 +14,8 @@ public class Lightning implements CommandExecutor  {
         if (sender instanceof Player) {
             Player player = (Player) sender;
             player.getWorld().strikeLightning(player.getLocation());
+        } else {
+            sender.sendMessage(ChatColor.RED + "You need to be a player to use this command");
         }
         return true;
     }
