05529586fbd74ef0c54adddd730bf5b6d5afd158
ShrimpyStuff <sajidmonowar11@gmail.com>
2020-12-13 13:57:13 -0500

    Moderate

--- src/main/java/ca/sajid/ultimateplugin/listeners/Moderating.java
+++ src/main/java/ca/sajid/ultimateplugin/listeners/Moderating.java
@@ -1,19 +1,32 @@
 package ca.sajid.ultimateplugin.listeners;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class Moderating implements Listener {
     @EventHandler
-    public void onChatMessage(AsyncPlayerChatEvent e) {
+    public void onChatMessage(AsyncPlayerChatEvent e) throws IOException {
         String message = e.getMessage();
-        String[] badWords = {"fuck", "shit"};
-        for (String word : badWords) {
-            int length = word.length();
-            String fixedWord = new String(new char[length]).replace("\0", "#");
-            String fixed = message.replaceAll(word, fixedWord);
-            e.setMessage(fixed);
-        }
+        String link = "https://www.purgomalum.com/service/json?text=" + message;
+        URL url = new URL(link);
+        URLConnection request = url.openConnection();
+        request.connect();
+
+        JsonParser jp = new JsonParser();
+        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
+        JsonObject rootObj = root.getAsJsonObject();
+        String text = rootObj.get("result").getAsString();
+
+        e.setMessage(text);
     }
 }
