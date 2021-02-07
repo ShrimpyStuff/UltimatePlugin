d385203485efaa3e69d52b91ca9884ea62f980e0
ElCholoGamer <josedanielgrayson@gmail.com>
2021-02-06 19:31:18 -0500

    add some command permissions

--- src/main/java/ca/sajid/ultimateplugin/commands/InventoryOpen.java
+++ src/main/java/ca/sajid/ultimateplugin/commands/InventoryOpen.java
@@ -11,11 +11,6 @@ public class InventoryOpen implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (!sender.isOp()) {
-            sender.sendMessage(ChatColor.RED + "You must be an operator to use this command");
-            return true;
-        }
-
         if (!(sender instanceof Player)) {
             sender.sendMessage("You must be a player to use this command");
             return true;
