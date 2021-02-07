9bc520c31bb46649761e15e21b8afb35e63db05f
ElCholoGamer <josedanielgrayson@gmail.com>
2021-02-07 11:09:11 -0500

    add Utils.color and update Utils.log

--- src/main/java/ca/sajid/ultimateplugin/Utils.java
+++ src/main/java/ca/sajid/ultimateplugin/Utils.java
@@ -9,17 +9,17 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Utils {
 
     private static final UltimatePlugin plugin = UltimatePlugin.getPlugin();
 
-    public static void log(Object msg, Object... optionalArgs) {
+    public static void log(Object s, Object... args) {
+        String prefix = "[" + plugin.getName() + "]";
         plugin.getServer().getConsoleSender().sendMessage(
-                ChatColor.translateAlternateColorCodes(
-                        '&',
-                        String.format(msg.toString(), optionalArgs)
-                )
+                prefix + " " + color(String.format(s.toString(), args))
         );
     }
 
@@ -29,5 +29,26 @@ public class Utils {
 
         InputStreamReader data = new InputStreamReader((InputStream) con.getContent());
         return new JsonParser().parse(data);
+    }
+
+    public static String color(String s) {
+        try {
+            // Check that class and method exist
+            Class.forName("net.md_5.bungee.api.ChatColor").getMethod("of", String.class);
+
+            // regex is cool isnt it
+            Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
+            Matcher matcher = pattern.matcher(s);
+
+            while (matcher.find()) {
+                String color = s.substring(matcher.start(), matcher.end());
+                s = s.replace(color, net.md_5.bungee.api.ChatColor.of(color).toString());
+
+                matcher = pattern.matcher(s);
+            }
+        } catch (ClassNotFoundException | NoSuchMethodException ignored) {
+        }
+
+        return ChatColor.translateAlternateColorCodes('&', s);
     }
 }
