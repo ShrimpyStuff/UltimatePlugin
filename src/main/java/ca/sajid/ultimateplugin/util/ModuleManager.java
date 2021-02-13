79a9f1953f3060e2971d6bba7da47f3919f51fbd
ElCholoGamer <josedanielgrayson@gmail.com>
2021-02-12 22:15:58 -0500

    return module on load

--- src/main/java/ca/sajid/ultimateplugin/util/ModuleManager.java
+++ src/main/java/ca/sajid/ultimateplugin/util/ModuleManager.java
@@ -9,15 +9,17 @@ public class ModuleManager {
     private final List<BaseModule> modules = new ArrayList<>();
 
     // idk why im using generics and reflect but hey it looks cool doesnt it
-    public <T extends BaseModule> void load(Class<T> clazz) {
+    public <T extends BaseModule> T load(Class<T> clazz) {
         try {
             Constructor<T> constructor = clazz.getConstructor();
             BaseModule module = constructor.newInstance();
             modules.add(module);
 
             module.onEnable();
+            return module;
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
     }
 
